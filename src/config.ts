b8530168b3270c25e0f975b54078c76efd65137d
Ruilin Xu <xurlin7@mail2.sysu.edu.cn>
2025-11-14 14:29:28 +0800

    feat(navbar): add Skaldheim link to the navigation bar

--- src/config.ts
+++ src/config.ts
@@ -49,6 +49,11 @@ export const navBarConfig: NavBarConfig = {
 			url: "https://github.com/yuchihatuntun/Stillpoint", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
+		{
+			name: "Skaldheim",
+			url: "https://www.skaldheim.cn/", // Internal links should not include the base path, as it is automatically added
+			external: true, // Show an external link icon and will open in a new tab
+		},
 	],
 };
 
